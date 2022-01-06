05e9b815307376f7edcab23654a67d16ea9270fa
Alan T. Cote' <alantcote@gmail.com>
2022-01-05 20:21:24 -0500

    Snap! Among other things, a change in utilities module name to clutilities.

--- DupFileFinder/src/main/java/module-info.java
+++ DupFileFinder/src/main/java/module-info.java
@@ -5,11 +5,11 @@ module dupfilefinder {
 	exports net.sf.cotelab.app.dupfilefinder.gui;
 	exports net.sf.cotelab.app.dupfilefinder.hunter;
 	exports net.sf.cotelab.app.dupfilefinder.tree;
-	
+
+	requires clutilities;
 	requires transitive java.desktop;
 	requires java.logging;
 	requires javafx.graphics;
 	requires javafx.swing;
 	requires javafx.web;
-	requires util;
 }
